93816edaf217136368b782ed1eb0eb6019196a25
Jörgen Nilsson [MVP] <jorgen@ccmexec.com>
2021-02-15 08:16:34 +0100

    Add files via upload
    Powershell script to set Computer Description during OSD

---
+++ SetComputerDesc.ps1
@@ -0,0 +1,16 @@
+﻿# Script to set the Computer description in AD for the computer on which it is executed from.
+# Example Command line Powershell.exe -Set-ExecutionPolicy bypass -file .\SetComputerDesc "Computer Description"
+
+[string]$Description = $args[0]
+
+    try {
+
+        $ComputerDn = ([ADSISEARCHER]"sAMAccountName=$($env:COMPUTERNAME)$").FindOne().Path
+        $ADComputer = [ADSI]$ComputerDn
+        $ADComputer.description = $Description
+        $ADComputer.SetInfo()
+
+    }
+    catch {
+        $_.Exception.Message ; Exit 1
+    }
